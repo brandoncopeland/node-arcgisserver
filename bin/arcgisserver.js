bfbc935883b489bec6b5966a32d52d4b0ffe45b8
Brandon Copeland <br@ndoncopeland.com>
2014-10-02 18:04:41 -0500

    adds displaying help with run with no command. issue #2

--- bin/arcgisserver.js
+++ bin/arcgisserver.js
@@ -6,11 +6,15 @@ var program = require('commander');
 
 program
   .version(packageJson.version)
-  .option('-h, --host <host>', 'Host name for the ArcGIS for Server site (ex. mysite:6080).')
-  .option('-u, --username <username>', 'User name for an administrative account on the ArcGIS for Server site.')
-  .option('-p, --password <password>', 'Credentials for the `username` account.');
+  .option('-h, --host <host>', 'host name for the ArcGIS for Server site (ex. mysite:6080).')
+  .option('-u, --username <username>', 'user name for an administrative account on the ArcGIS for Server site.')
+  .option('-p, --password <password>', 'credentials for the `username` administrative account.');
 
 require('../lib/commands/command-test');
 require('../lib/commands/command-servicecount');
 
 program.parse(process.argv);
+
+if (!program.args.length) {
+  program.help();
+}
